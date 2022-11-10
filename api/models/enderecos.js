8e6898f70ee74be71aab9043c73fcf8667b8e9e7
carlos garcia <chenzhen.dev@gmail.com>
2022-11-10 02:46:54 -0300

    Criando modelo para entidade/tabela endereco...

---
+++ api/models/enderecos.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class enderecos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  enderecos.init({
+    cep: DataTypes.STRING,
+    logradouro: DataTypes.STRING,
+    numero: DataTypes.STRING,
+    bairro: DataTypes.STRING,
+    cidade: DataTypes.STRING,
+    estado: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'enderecos',
+  });
+  return enderecos;
+};
