79e23c05a4c27fc3210a04594225aa3e4c78ef27
carlos garcia <chenzhen.dev@gmail.com>
2022-11-10 04:42:38 -0300

    Corrigindo relacionamento da entidade/tabela endereco...

--- api/models/enderecos.js
+++ api/models/enderecos.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       enderecos.belongsTo(models.fornecedores, {
-        foreignKey: 'fk_fornecedores_enderecos',
+        foreignKey: 'fornecedor',
         type: DataTypes.INTEGER
       })
     }
